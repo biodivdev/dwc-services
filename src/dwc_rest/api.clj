c8a440698df9ae71dcea36414fd68393ade66c2a
Diogo Souza <diogo@diogok.net>
2014-03-10 15:31:38 -0300

    initial convert api

--- src/dwc_rest/api.clj
+++ src/dwc_rest/api.clj
@@ -1,18 +1,49 @@
 (ns dwc-rest.api
+  (:require [clj-http.client :as http])
   (:use dwc-rest.core)
-  (:use compojure.core))
+  (:use compojure.core)
+  (:use ring.util.response))
 
-(def formats ["dwc-a" "csv" "tsv" "ods" "xlsx" "json" "geojson"])
+(def ctypes 
+  {:json "application/json"
+   :geojson "application/json"
+   :csv "text/csv"
+   :tsv "text/tsv"
+   :dwca "application/zip"
+   :archive "application/zip"
+   :xlsx "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"})
 
-(def params ["from" "to" "url" "hook" "stream"])
+(defn res
+  ""
+  [params]
+  (try
+    (if-let [url (:url params)]
+      (if-let [from (:from params)]
+        (if-let [to (:to params)]
+          (if-let [hook (:hook params)]
+            (do 
+              (future (http/post {:body (convert (:from from :to to :source url))
+                                  :headers {"Content-Type" (get ctypes to)}}))
+              {:status 200 :body "ok"})
+            {:status 200
+             :headers {"Content-Disposition" (str "attachment; filename=\"dwc." to "\"")
+                       "Content-Type" (get ctypes to)}
+             :body (convert {:from from :to to :source url})})
+          {:status 400 :body "Must provide 'to' parameter of the output format."})
+        {:status 400 :body "Must provide 'from' parameter of the input format."})
+     {:status 400 :body "Must provide 'url' parameter of data source."})
+    (catch Exception e 
+      {:status 500 :body (str "Server error: " (.getMessage e)) })
+    )
+  )
 
 (defroutes  api-v1-routes
 
-  (GET "/convert" {params :params}
-    nil)
+  (GET "/convert" req
+   (res (:params req)))
 
-  (POST "/convert" data
-    nil)
-  
-  )
+  (POST "/convert" req
+    (res (assoc (:params req) :url (:body req))))
+
+)
 
