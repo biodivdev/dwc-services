149527acf4a00c64f49d471efab21ec64b66b456
Diogo Souza <diogo@diogok.net>
2014-03-11 17:03:34 -0300

    little refactoring on api

--- src/dwc_rest/api.clj
+++ src/dwc_rest/api.clj
@@ -13,39 +13,41 @@
    :archive "application/zip"
    :xlsx "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"})
 
-(defn res
+(defn safe
+  ""
+  [fun] 
+   (try (fun)
+    (catch Exception e 
+      (do (.printStackTrace e)
+        {:status 500 :body (str "Server error: " (.getMessage e))}))))
+
+(defn convert-api
   ""
   [params]
-  (try
     (if-let [url (:url params)]
       (if-let [from (:from params)]
         (if-let [to (:to params)]
           (if-let [hook (:hook params)]
             (do 
               (future
-                (http/post hook {:body (convert {:from from :to to :source url})
+                (http/post hook {:body (convert {:from from :to to :source url :fixes (:fixes params)})
                                   :headers {"Content-Type" (get ctypes to)}}))
               {:status 200 :body "ok"})
             {:status 200
              :headers {"Content-Disposition" (str "attachment; filename=\"dwc." to "\"")
                        "Content-Type" (get ctypes to)}
-             :body (convert {:from from :to to :source url})})
+             :body (convert {:from from :to to :source url :fixes (:fixes params)})})
           {:status 400 :body "Must provide 'to' parameter of the output format."})
         {:status 400 :body "Must provide 'from' parameter of the input format."})
-     {:status 400 :body "Must provide 'url' parameter of data source."})
-    (catch Exception e 
-      (do (.printStackTrace e)
-        {:status 500 :body (str "Server error: " (.getMessage e))}))
-    )
-  )
+     {:status 400 :body "Must provide 'url' parameter of data source or data as input."}))
 
 (defroutes  api-v1-routes
 
   (GET "/convert" req
-   (res (:params req)))
+   (safe #(convert-api (:params req))))
 
   (POST "/convert" req
-    (res (assoc (:params req) :url (:body req))))
+    (safe #(convert-api (assoc (:params req) :url (:body req)))))
 
 )
 
