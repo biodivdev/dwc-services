489684fd5e25ba71a5473fdc995d6bc83a2a2023
Diogo Souza <diogo@diogok.net>
2014-03-11 17:03:16 -0300

    adding possible normalization on occurrences

--- src/dwc_rest/core.clj
+++ src/dwc_rest/core.clj
@@ -23,8 +23,56 @@
      (spit tmp data)
      (.getAbsolutePath tmp)))
 
+(defn coord2decimal
+  [coord] 
+   (let [coord (rest (re-find #"^([\d]+)[^\d]+([\d]+)[^\d]+([\d]+)[^\d]*([\w]{1})" coord))]
+     (*
+       (float
+       (+ (Integer/valueOf (nth coord 0))
+          (/ (Integer/valueOf (nth coord 1)) 60)
+          (/ (Integer/valueOf (nth coord 2)) 3600)
+       ))
+       (if (or 
+             (= "w" (nth coord 3))
+             (= "W" (nth coord 3))
+             (= "s" (nth coord 3))
+             (= "S" (nth coord 3)))
+         -1 1))))
+
+(defn fix-coords
+  [occ] 
+   (let [lat (:latitude occ)
+         lng (:longitude occ)
+         decLat (:decimalLatitude occ)
+         decLng (:decimalLongitude occ)]
+     (if (and (not (nil? decLat)) (not (nil? decLng)))
+       occ
+       (if (and (not (nil? lat)) (not (nil? lng)))
+         (assoc occ :decimalLatitude (coord2decimal lat) :decimalLongitude (coord2decimal lng))
+         occ)
+       )
+   ))
+
+(defn fix-id
+  [occ] 
+   (if-not (nil? (:occurrenceID occ)) occ
+     (if-not (nil? (:id occ)) (assoc occ :occurrenceID (:id occ))
+       (if (and (not (nil? (:institutionCode occ)))
+                (not (nil? (:collectionCode occ)))
+                (not (nil? (:catalogNumber occ))) )
+          (assoc occ :occurrenceID (str "urn:occurrence:" 
+                                        (:institutionCode occ) ":" 
+                                        (:collectionCode occ) ":"
+                                        (:catalogNumber occ)))
+         (assoc occ :occurrenceID (str "urn:occurrence:" (java.util.UUID/randomUUID)))))))
+
 (defn convert
-  [{from :from to :to source :source}]
+  [{from :from to :to source :source fixes :fixes}]
    ((get writer (keyword to ))
-     ((get reader (keyword from )) source)))
+    (let [data ((get reader (keyword from)) source)]
+      (if-not fixes
+       data
+       (->> ((get reader (keyword from )) source)
+            (map fix-coords)
+            (map fix-id))))))
 
